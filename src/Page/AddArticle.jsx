f1dfc7cb6577549248d1f463d9e4c707cff32908
Lofipure <littlebanana@126.com>
2020-09-13 00:13:01 +0800

    完成初始化布局(保命睡觉)

---
+++ src/Page/AddArticle.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import marked from "marked";
+import { Row, Col, Input, Select, Button, DatePicker } from "antd";
+
+import "../Style/AddArticle.css";
+
+const { Option } = Select;
+const { TextArea } = Input;
+
+const AddArticle = () => {
+  return <div>Add Article</div>;
+};
+export default AddArticle;
